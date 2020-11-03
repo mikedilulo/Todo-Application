85c416671ada2e7b55c3b6f010aee24200eca079
mikedilulo <mikedilulo2019@gmail.com>
2020-11-02 18:26:00 -0700

    Setup for Todo Aoplication

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className="todo">
+      <h1>Todo Component</h1>
+    </div>
+  )
+}
+
+export default Todo
---
+++ src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoForm = () => {
+  return (
+    <div className="todo-form">
+      <input type="text" />
+      <button>Add Todo</button>
+    </div>
+  )
+}
+
+export default TodoForm
